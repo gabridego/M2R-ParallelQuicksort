84021d4775c1f057433db356aad5d2ffd5a93ee2
gabridego <superbrio97@gmail.com>
2021-11-18 16:26:40 +0100

    update

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -4,8 +4,8 @@ mkdir -p $OUTPUT_DIRECTORY
 OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
 
 touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 200000 400000 600000 800000 1000000; do
-    for rep in `seq 1 5`; do
+for i in 100 1000 10000 100000 200000 400000 600000 800000 1000000 5000000 10000000; do
+    for rep in `seq 1 20`; do
         echo "Size: $i" >> $OUTPUT_FILE;
         ./src/parallelQuicksort $i >> $OUTPUT_FILE;
     done ;
