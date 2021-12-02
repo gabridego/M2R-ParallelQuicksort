a511302b6222e7b09016411c3db5909a0f9904ac
gabridego <superbrio97@gmail.com>
2021-12-02 10:25:07 +0100

    add metadata, tests on different machine

--- scripts/run_benchmarking.py
+++ scripts/run_benchmarking.py
@@ -1,25 +1,61 @@
-import sys
 import os
+import sys
 import subprocess
+import time
+import math
 import random
-from datetime import date, datetime
+from pathlib import Path
+from decimal import Decimal
 
-output_directory = os.path.join("data", os.uname()[1] + '_' + date.today().strftime("%Y-%m-%d"))
-os.makedirs(output_directory, exist_ok=True)
-output_file = os.path.join(output_directory, "measurements_" + datetime.now().strftime("%H_%M") + ".txt")
-output_csv = os.path.splitext(output_file)[0] + ".csv"
+from datetime import datetime
 
-sizes = []
-for size in [100, 1000, 10000, 100000, 200000, 400000, 600000, 800000, 1000000]:
-	sizes.extend([str(size)] * 20)
-random.shuffle(sizes)
+import cpuinfo
 
-with open(output_file, 'w') as f:
-	while len(sizes) != 0:
-		size = sizes.pop()
-		subprocess.run(["echo", f"Size: {size}"], stdout=f)
-		subprocess.run(["./src/parallelQuicksort", size], stdout=f)
 
-with open(output_file, 'r') as fin:
-	with open(output_csv, 'w') as fout:
-		subprocess.run(["perl", "scripts/csv_quicksort_extractor.pl"], stdin=fin, stdout=fout)
+def run(start, end, step, reps=20):
+    output_dir = f'data/{os.uname()[1]}_{datetime.today().strftime("%Y-%m-%d")}'
+    os.makedirs(output_dir, exist_ok=True)
+    output_file = f'{output_dir}/measurements_{datetime.now().strftime("%H_%M")}'
+    output_file_text = f'{output_file}.txt'
+    output_file_csv = f'{output_file}.csv'
+
+    Path(output_file_text).touch()
+
+    items = list(range(start, end, step))
+    items.append(end)
+    items = items * reps
+    random.shuffle(items)
+    num_len = len(items)
+
+    for i, item in enumerate(items):
+        print(f'[{i+1}/{num_len}]: {"{:.2E}".format(Decimal(item))}')
+        with open(output_file_text, 'a') as file:
+            file.write(f'Size: {item}\n')
+        subprocess.run(f"./src/parallelQuicksort {item} >> {output_file_text}", shell=True)
+
+    cpuinfos = cpuinfo.get_cpu_info()
+    os.setxattr(output_file_text, 'user.cpu', f'{cpuinfos["brand_raw"]}'.encode())
+    os.setxattr(output_file_text, 'user.cores', f'{os.cpu_count()}'.encode())
+    os.setxattr(output_file_text, 'user.ram', f'{os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES") / (1024.**3)}'.encode())
+    os.setxattr(output_file_text, 'user.start', f'{start}'.encode())
+    os.setxattr(output_file_text, 'user.end', f'{end}'.encode())
+    os.setxattr(output_file_text, 'user.step', f'{step}'.encode())
+    os.setxattr(output_file_text, 'user.reps', f'{reps}'.encode())
+
+    subprocess.run(f"perl scripts/csv_quicksort_extractor.pl < {output_file_text} > {output_file_csv}", shell=True)
+
+if __name__ == '__main__':
+    argslen = len(sys.argv)
+    if argslen < 4:
+        print("Error: Please provide start, end and step arguments")
+        sys.exit()
+
+    start = int(float(sys.argv[1]))
+    end = int(float(sys.argv[2]))
+    step = int(float(sys.argv[3]))
+    reps = 20
+
+    if argslen == 5:
+        reps = int(float(sys.argv[4]))
+
+    run(start, end, step, reps)
